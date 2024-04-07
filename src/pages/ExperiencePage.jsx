74684f6320df0a33d8311f634b8f37a992dfa597
mks1313 <139466229+mks1313@users.noreply.github.com>
2024-04-08 00:06:31 +0200

    experience

--- src/pages/ExperiencePage.jsx
+++ src/pages/ExperiencePage.jsx
@@ -4,12 +4,29 @@ import Letters from "../components/Letters";
 const ExperiencePage = () => {
     const { t } = useTranslation();
     return (
-        <div className="flex flex-col md:flex-row justify-between bg-black text-gray-300 lg:pl-8">
-            <div className="px-4 lg:pl-80 bg-black mt-6">
-                <Letters value={t('experience.title')} size="xl" underline={false} gradient={true} className="text-center text-white mb-4" id="title" />
-                <Letters value={t('experience.description')} size="l" underline={false} gradient={true} className="text-center text-white" id="description" />
+        <div className="flex flex-col md:flex-col justify-between bg-black text-gray-300 lg:pl-8">
+            <div className="px-4 lg:pl-80 bg-black mt-6 mb-8 ">
+                <Letters value={t('experience.title1')} size="xl" underline={false} gradient={true} className="text-center text-white mb-2" id="title" />
+                <Letters value={t('experience.date1')} size="s" underline={false} gradient={true} className="text-center text-white mb-2" id="date" />
+                <Letters value={t('experience.description1')} size="l" underline={false} gradient={true} className="text-center text-white" id="description" />
             </div>
+            <div className="w-full mx-4">
+        <hr className="bg-gray-100 h-px mt-4 mb-8" />
+    </div>
+            <div className="px-4 lg:pl-80 bg-black mt-6 mb-8 ">
+                <Letters value={t('experience.title2')} size="xl" underline={false} gradient={true} className="text-center text-white mb-2" id="title" />
+                <Letters value={t('experience.date2')} size="s" underline={false} gradient={true} className="text-center text-white mb-2" id="date" />
+                <Letters value={t('experience.description2')} size="l" underline={false} gradient={true} className="text-center text-white" id="description" />
+            </div>
+            <hr className="bg-gray-100 h-px mt-4 mb-8" />
+            <div className="px-4 lg:pl-80 bg-black mt-6 mb-8 ">
+                <Letters value={t('experience.title3')} size="xl" underline={false} gradient={true} className="text-center text-white mb-2" id="title" />
+                <Letters value={t('experience.date3')} size="s" underline={false} gradient={true} className="text-center text-white mb-2" id="date" />
+                <Letters value={t('experience.description3')} size="l" underline={false} gradient={true} className="text-center text-white" id="description" />
+            </div>
+            <hr className="bg-gray-100 h-px mt-4 mb-8" />
         </div>
+        
     )
 }
 
