5bd2e62e9eaea7c9d35a78c95d25396a2dbb3adf
mks1313 <139466229+mks1313@users.noreply.github.com>
2024-04-07 23:09:22 +0200

    enlaces-projects

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,19 @@
-const Button = ({ href, imageSrc, alt }) => {
+const Button = ({ href, imageSrc, alt, text, classname }) => {
     return (
-        <a href={href} target="_blank" rel="noopener noreferrer">
-            <button type="button" className="xl:w-[60px] xl:h-[60px] w-[50px] h-[50px] silver-gradient flex justify-center items-center border-2 border-gray-200 rounded-lg">
-                <img src={imageSrc} alt={alt} className="w-6 h-6" />
-            </button>
-        </a>
+      <a href={href} target="_blank" rel="noopener noreferrer">
+        <button
+          type="button"
+          className={`xl:w-[60px] xl:h-[60px] w-[50px] h-[50px] silver-gradient flex justify-center items-center border-2 border-gray-200 rounded-lg ${classname}`}
+        >
+          {imageSrc ? (
+            <img src={imageSrc} alt={alt} className="w-6 h-6" />
+          ) : (
+            text
+          )}
+        </button>
+      </a>
     );
-};
-
-export default Button;
+  };
+  
+  export default Button;
+  
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -8,26 +8,37 @@ const Projects = () => {
     return (
         <div className="flex flex-col md:flex-col justify-between bg-black text-gray-300 lg:pl-8">
             <div className="px-4 lg:pl-80 bg-black mt-6 mb-8">
-                <div className="flex justify-start items-center">
-                    <div className="flex flex-col items-center">
-                        <Button href={"https://github.com/mks1313/project-3-client"} imageSrc={github} alt="GitHub-client" />
-                        <span className="text-white mt-2">Client</span>
-                    </div>
-                    <div className="flex flex-col items-center ml-4">
-                        <Button href={"https://github.com/mks1313/project-3-server"} imageSrc={github} alt="GitHub-server" />
-                        <span className="text-white mt-2">Server</span>
-                    </div>
-                </div>
+            <div className="flex justify-start items-center">
+  <div className="flex flex-col items-center" style={{ height: "100px" }}> 
+    <Button href={"https://github.com/mks1313/project-3-client"} imageSrc={github} alt="GitHub-client" />
+    <span className="text-white mt-2">Client</span>
+  </div>
+  <div className="flex flex-col items-center ml-4" style={{ height: "100px" }}> 
+    <Button href={"https://github.com/mks1313/project-3-server"} imageSrc={github} alt="GitHub-server" />
+    <span className="text-white mt-2">Server</span>
+  </div>
+  <div className="flex flex-col items-center ml-4" style={{ height: "100px" }}> 
+    <Button href={"https://project-3-client.vercel.app/"} text="Visit" classname="custom-button" />
+  </div>
+</div>
+
                 <Letters value={t('projects.title1')} size="xl" underline={false} gradient={true} className="text-center text-white mb-2" id="title1" />
                 <Letters value={t('projects.date1')} size="s" underline={false} gradient={true} className="text-center text-white mb-2" id="date1" />
-                <img src={t('projects.image1')} alt="Imagen de web" className="w-90 h-60 mb-8 mx-auto mt-4" />
+                <img src={t('projects.image1')} alt="Imagen de web" className="w-90 h-60 mb-8 mx-auto mt-4 rounded-lg border-[6px] border-gray-200" />
                 <Letters value={t('projects.description1')} size="l" underline={false} gradient={true} className="text-center text-white" id="description1" />
             </div>
             <div className="w-full mx-4">
                 <hr className="bg-gray-100 h-px mt-4 mb-8" />
             </div>
             <div className="px-4 lg:pl-80 bg-black mt-6 mb-8">
-                <Button href={"https://github.com/mks1313/Historical-events-CRUD"} imageSrc={github} alt="GitHub-Historical-events-CRUD" />
+                <div className="flex justify-start items-center">
+                    <div className="flex flex-col items-center mr-4">
+                        <Button href={"https://github.com/mks1313/Historical-events-CRUD"} imageSrc={github} alt="GitHub-Historical-events-CRUD" />
+                    </div>
+                    <div className="flex flex-col items-center">
+                        <Button href={"https://historical-events-crud.vercel.app/"} text="Visit" classname="custom-button" />
+                    </div>
+                </div>
                 <Letters value={t('projects.title')} size="xl" underline={false} gradient={true} className="text-center text-white mb-2" id="title" />
                 <Letters value={t('projects.date')} size="s" underline={false} gradient={true} className="text-center text-white mb-2" id="date" />
                 <img src={t('projects.image')} alt="Imagen de web" className="w-90 h-60 mb-8 mx-auto mt-4 rounded-lg border-[6px] border-gray-200" />
