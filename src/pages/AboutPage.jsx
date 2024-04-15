17ab41642b30e5335802679e64fe97b840e98b88
mks1313 <139466229+mks1313@users.noreply.github.com>
2024-04-15 12:05:39 +0200

    estilo-letters

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -8,7 +8,7 @@ const AboutPage = () => {
         <div className="bg-black text-white min-h-screen flex justify-center items-center">
             <div className="px-4 lg:pl-80 bg-black mt-6">
             <Letters value={t('about.about')} size="xl" underline={true} gradient={true} className="text-center text-white mb-2" id="about" />
-                <Letters value={t('about.content')} size="xl" underline={false} gradient={true} className="text-center" id="about-content" />
+                <Letters value={t('about.content')} size="l" underline={false} gradient={true} className="text-center" id="about-content" />
             </div>
         </div>
     );
