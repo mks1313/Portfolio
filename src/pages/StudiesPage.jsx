b7a10b24777e5ed4ec2e66421d67f6868d37ad2a
mks1313 <139466229+mks1313@users.noreply.github.com>
2024-06-08 23:42:58 +0200

    agregado certificado de freeBootCamo

--- src/pages/StudiesPage.jsx
+++ src/pages/StudiesPage.jsx
@@ -5,12 +5,22 @@ const StudiesPage = () => {
     const { t } = useTranslation();
     return (
         <div className="flex flex-col md:flex-col justify-between bg-black text-gray-300 lg:pl-8">
+         <div className="px-4 lg:pl-80 bg-black mt-6 mb-8 ">
+                <Letters value={t('studies.title2')} size="xl" underline={false} gradient={true} className="text-center text-white mb-2" id="title" />
+                <Letters value={t('studies.date2')} size="s" underline={false} gradient={true} className="text-center text-white mb-2" id="date" />
+                <img src={t('studies.image1')} alt="certificado" className="w-90 h-60 mb-8 mx-auto mt-4 rounded-lg border-[6px] border-gray-200"/>
+                <Letters value={t('studies.description2')} size="l" underline={false} gradient={true} className="text-center text-white" id="description" />
+                <Letters value={t('studies.subdescription1')} size="m" underline={false} gradient={true} className="text-center text-white" id="subdescription" />
+            </div>
+            <div className="w-full mx-4">
+        <hr className="bg-gray-100 h-px mt-4 mb-8" />
+    </div>
             <div className="px-4 lg:pl-80 bg-black mt-6 mb-8 ">
                 <Letters value={t('studies.title')} size="xl" underline={false} gradient={true} className="text-center text-white mb-2" id="title" />
                 <Letters value={t('studies.date')} size="s" underline={false} gradient={true} className="text-center text-white mb-2" id="date" />
                 <img src={t('studies.image')} alt="Foto de perfil"className="w-90 h-60 mb-8 mx-auto mt-4 rounded-lg border-[6px] border-gray-200"/>
 
-                <Letters value={t('studies.description')} size="l" underline={false} gradient={true} className="text-center text-white" id="description" />
+                <Letters value={t('studies.description')} size="m" underline={false} gradient={true} className="text-center text-white" id="description" />
             </div>
             <div className="w-full mx-4">
         <hr className="bg-gray-100 h-px mt-4 mb-8" />
@@ -21,7 +31,10 @@ const StudiesPage = () => {
                 <Letters value={t('studies.description1')} size="l" underline={false} gradient={true} className="text-center text-white" id="description" />
                 <Letters value={t('studies.subdescription')} size="m" underline={false} gradient={true} className="text-center text-white" id="subdescription" />
             </div>
-            <hr className="md:hidden bg-gray-100 w-full border-0 h-px" />
+            <div className="w-full mx-4">
+        <hr className="bg-gray-100 h-px mt-4 mb-8" />
+    </div>
+       
         </div>
     )
 }
