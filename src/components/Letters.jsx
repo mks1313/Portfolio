c3f122f3359433062ef59568f5a26f30f740132e
mks1313 <mg.marinov@gmx.es>
2025-01-02 23:37:55 +0100

    mejoras menores

--- src/components/Letters.jsx
+++ src/components/Letters.jsx
@@ -1,35 +1,47 @@
-const Letters = ({ value, size, underline = false, gradient = false, className = '', id }) => {
-    let fontSize = '';
+const Letters = ({
+  value,
+  size,
+  underline = false,
+  gradient = false,
+  className = "",
+  id,
+}) => {
+  let fontSize = "";
 
-    switch (size) {
-        case "xl":
-            fontSize = 'xl:text-[36px] lg:text-[32px] md:text-[28px] text-[24px] font-bold';
-            break;
-        case 'l':
-            fontSize = 'xl:text-[32px] lg:text-[28px] md:text-[24px] text-[20px] font-medium';
-            break;
-        case "m":
-            fontSize = 'xl:text-[20px] lg:text-[18px] md:text-[16px] text-[14px]';
-            break;
-        case "s":
-            fontSize = 'xl:text-[12px] lg:text-[10px] md:text-[9px] text-[8px]';
-            break;
-        default:
-            fontSize = '';
-    }
+  switch (size) {
+    case "xl":
+      fontSize =
+        "xl:text-[36px] lg:text-[32px] md:text-[28px] text-[24px] font-bold";
+      break;
+    case "l":
+      fontSize =
+        "xl:text-[28px] lg:text-[24px] md:text-[20px] text-[16px] font-medium";
+      break;
+    case "m":
+      fontSize = "xl:text-[20px] lg:text-[18px] md:text-[16px] text-[14px]";
+      break;
+    case "s":
+      fontSize = "xl:text-[12px] lg:text-[10px] md:text-[9px] text-[8px]";
+      break;
+    default:
+      fontSize = "";
+  }
 
-    const gradientClass = gradient ? 'silver-gradient' : 'text-[#ECECEC]';
+  const gradientClass = gradient ? "silver-gradient" : "text-[#ECECEC]";
 
-    return (
-        <p id={id} className={`
+  return (
+    <p
+      id={id}
+      className={`
             ${gradientClass}
-            ${underline ? 'underline' : ''}
+            ${underline ? "underline" : ""}
             ${fontSize}
             ${className}
-        `}>
-            {value}
-        </p>
-    );
+        `}
+    >
+      {value}
+    </p>
+  );
 };
 
 export default Letters;
