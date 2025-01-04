b542f251c58c72f9e1c68356306cb104e2e70d07
mks1313 <mg.marinov@gmx.es>
2025-01-04 08:56:39 +0100

    dup

--- src/pages/ExperiencePage.jsx
+++ src/pages/ExperiencePage.jsx
@@ -7,88 +7,23 @@ const ExperiencePage = () => {
   return (
     <div className="bg-black text-gray-300">
       <div className="px-4 lg:pl-80 bg-black mt-6 mb-8">
-        <Letters
-          value={t("experience.title1")}
-          size="xl"
-          underline={false}
-          gradient={true}
-          className="text-center text-white mb-2 animate-fadeIn"
-          id="title"
-        />
-        <Letters
-          value={t("experience.date1")}
-          size="s"
-          underline={false}
-          gradient={true}
-          className="text-center text-white mb-2 animate-fadeIn"
-          id="date"
-        />
-        <Letters
-          value={t("experience.description1")}
-          size="l"
-          underline={false}
-          gradient={true}
-          className="text-center text-white animate-slideIn"
-          id="description"
-        />
+        <Letters value={t("experience.title1")} size="xl" underline={false} gradient={true} className="text-center text-white mb-2 animate-fadeIn" id="title" />
+        <Letters value={t("experience.date1")} size="s" underline={false} gradient={true} className="text-center text-white mb-2 animate-fadeIn" id="date" />
+        <Letters value={t("experience.description1")} size="l" underline={false} gradient={true} className="text-center text-white animate-slideIn" id="description" />
       </div>
       <div className="w-full mx-4">
         <hr className="bg-gray-100 h-px mt-4 mb-8" />
       </div>
-
       <div className="px-4 lg:pl-80 bg-black mt-6 mb-8">
-        <Letters
-          value={t("experience.title2")}
-          size="xl"
-          underline={false}
-          gradient={true}
-          className="text-center text-white mb-2 animate-fadeIn"
-          id="title"
-        />
-        <Letters
-          value={t("experience.date2")}
-          size="s"
-          underline={false}
-          gradient={true}
-          className="text-center text-white mb-2 animate-fadeIn"
-          id="date"
-        />
-        <Letters
-          value={t("experience.description2")}
-          size="l"
-          underline={false}
-          gradient={true}
-          className="text-center text-white animate-slideIn"
-          id="description"
-        />
+        <Letters value={t("experience.title2")} size="xl" underline={false} gradient={true} className="text-center text-white mb-2 animate-fadeIn" id="title" />
+        <Letters value={t("experience.date2")} size="s" underline={false} gradient={true} className="text-center text-white mb-2 animate-fadeIn" id="date" />
+        <Letters value={t("experience.description2")} size="l" underline={false} gradient={true} className="text-center text-white animate-slideIn" id="description" />
       </div>
       <hr className="bg-gray-100 h-px mt-4 mb-8" />
-
       <div className="px-4 lg:pl-80 bg-black mt-6 mb-8">
-        <Letters
-          value={t("experience.title3")}
-          size="xl"
-          underline={false}
-          gradient={true}
-          className="text-center text-white mb-2 animate-fadeIn"
-          id="title"
-        />
-        <Letters
-          value={t("experience.date3")}
-          size="s"
-          underline={false}
-          gradient={true}
-          className="text-center text-white mb-2 animate-fadeIn"
-          id="date"
-        />
-        <Letters
-          value={t("experience.description3")}
-          size="l"
-          underline={false}
-          gradient={true}
-          className="text-center text-white animate-slideIn"
-          id="description"
-        />
+        <Letters value={t("experience.title3")} size="xl" underline={false} gradient={true} className="text-center text-white mb-2 animate-fadeIn" id="title" />
+        <Letters value={t("experience.date3")} size="s" underline={false} gradient={true} className="text-center text-white mb-2 animate-fadeIn" id="date" />
+        <Letters value={t("experience.description3")} size="l" underline={false} gradient={true} className="text-center text-white animate-slideIn" id="description" />
       </div>
       <hr className="bg-gray-100 h-px mt-4 mb-8" />
     </div>
