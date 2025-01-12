7cf0e4afb95289ec6f8b969a21de6dc1714e2541
mks1313 <mg.marinov@gmx.es>
2025-01-12 09:14:24 +0100

    fixed image and font size

--- src/components/Letters.jsx
+++ src/components/Letters.jsx
@@ -11,17 +11,17 @@ const Letters = ({
   switch (size) {
     case "xl":
       fontSize =
-        "text-[36px] lg:text-[32px] md:text-[28px] text-[24px] font-bold";
+        "xl:text-[36px] lg:text-[32px] md:text-[28px] text-[24px] font-bold";
       break;
     case "l":
       fontSize =
-        "text-[20px] md:text-[16px] lg:text-[18px] xl:text-[20px]";
+        "xl:text-[28px] lg:text-[24px] md:text-[20px] text-[16px] font-medium";
       break;
     case "m":
-      fontSize = "text-[20px] lg:text-[18px] md:text-[16px] text-[14px]";
+      fontSize = "xl:text-[20px] lg:text-[18px] md:text-[16px] text-[14px]";
       break;
     case "s":
-      fontSize = "text-[12px] lg:text-[10px] md:text-[9px] text-[8px]";
+      fontSize = "xl:text-[12px] lg:text-[10px] md:text-[9px] text-[8px]";
       break;
     default:
       fontSize = "";
