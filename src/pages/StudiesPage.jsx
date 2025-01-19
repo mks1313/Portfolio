c281c9514a86183fb1788a64d6102fdd7d364f49
mks1313 <mg.marinov@gmx.es>
2025-01-19 23:36:47 +0100

    added certification tester

--- src/pages/StudiesPage.jsx
+++ src/pages/StudiesPage.jsx
@@ -9,6 +9,18 @@ const StudiesPage = () => {
       <div className="absolute inset-0 bg-black bg-opacity-70 z-10"></div>
       
       <div className="relative z-20">
+      <StudySection
+          title={t("studies.title4")}
+          date={t("studies.date4")}
+          description={t("studies.description4")}
+          image={t("studies.image3")}
+          altText="udemy certificate"
+          animation="animate__fadeInLeft"
+        />
+        
+        <div className="w-full mx-4">
+          <hr className="bg-gray-100 h-px mt-4 mb-8" />
+        </div>
         <StudySection
           title={t("studies.title3")}
           date={t("studies.date3")}
